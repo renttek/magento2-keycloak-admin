878240e869cbcdb48667ddca6116db13cdabf5f4
Julian Nuß <jn@integer-net.de>
2023-09-28 17:12:45 +0200

    Register users on login

--- TODO.md
+++ TODO.md
@@ -1,10 +1,5 @@
 # TODO
 
-- Automatically Login if user with matching email-address is found
-- Register new user if no user with matching email address is found
-  - Filter registration based on email allow-list/pattern
-  - Set magento user role based on keycloak realm roles (mapping)
-- Handle access token expiration
-  - Save refresh token in user session
-  - refresh token on every backend request
-  - automatically logout if token is no longer valid / can not be refreshed
+- Filter registration based on email allow-list/pattern
+- Set magento user role based on keycloak realm roles (mapping)
+- Map admin user properties base don keycloak attributes
--- src/Service/AdminUser.php
+++ src/Service/AdminUser.php
@@ -8,20 +8,23 @@ use Magento\User\Model\ResourceModel\User\Collection;
 use Magento\User\Model\ResourceModel\User\CollectionFactory;
 use Magento\User\Model\User;
 use Magento\User\Model\UserFactory;
+use Magento\User\Model\ResourceModel\User as ResourceUser;
 use Stevenmaguire\OAuth2\Client\Provider\KeycloakResourceOwner;
+use Symfony\Component\String\ByteString;
 
 class AdminUser
 {
     public function __construct(
         private readonly CollectionFactory $userCollection,
         private readonly UserFactory $userFactory,
+        private readonly ResourceUser $userResource,
     ) {
     }
 
     public function getOrCreateUser(KeycloakResourceOwner $keycloakResourceOwner): User
     {
         $email = $keycloakResourceOwner->getEmail();
-        $user  = $this->getUserByEmail($email);
+        $user  = $this->getUserByEmail($email) ?? $this->createUser($keycloakResourceOwner);
 
         if ($user === null) {
             dd(__LINE__);
@@ -43,5 +46,30 @@ class AdminUser
         return $user->getId() !== null
             ? $user
             : null;
+    }
+
+    private function createUser(KeycloakResourceOwner $keycloakResourceOwner): User
+    {
+        [
+            'email' => $email,
+            'name' => $username,
+            'given_name' => $firstname,
+            'family_name' => $lastname,
+        ] = $keycloakResourceOwner->toArray();
+
+        /** @var User $user */
+        $user = $this->userFactory->create();
+        $user->setUserName($username);
+        $user->setEmail($email);
+        $user->setFirstName($firstname);
+        $user->setLastName($lastname);
+        $user->setRoleId(1); // TODO: use a mapping between keycloak realm roles and magento roles
+        $user->setInterfaceLocale('en_US'); // TODO: map using a keycloak attribute
+        $user->setIsActive(true);
+        $user->setPassword(ByteString::fromRandom(64)->toString());
+
+        $this->userResource->save($user);
+
+        return $this->getUserByEmail($email);
     }
 }
